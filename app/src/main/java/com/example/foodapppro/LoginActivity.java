18a30b5ba217f79f07dd08a7ba1a78f0fb3ac16f
Diana Ghrayeb <80783730+diana-wael@users.noreply.github.com>
2022-05-20 16:04:47 +0300

    neew

--- app/src/main/java/com/example/foodapppro/LoginActivity.java
+++ app/src/main/java/com/example/foodapppro/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(LoginActivity.this,RegistrationActivity.class));
 
     }
 }
